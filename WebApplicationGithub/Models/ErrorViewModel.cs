18d6e5f5725695bc6820262b62807c4a173dfe33
afolesmith <afolesmith@gmail.com>
2020-11-30 15:58:14 +0100

    update project

---
+++ WebApplicationGithub/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationGithub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
